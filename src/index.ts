96907d7ae8f9334b15199692709b9fa7ee6fbb2e
Fernando Nicolás <fersanti2896@gmail.com>
2022-07-25 13:43:00 -0500

    Aplicando genericos en getPokemon.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+import { getPokemon } from "./generics/getPokemon";
 
+getPokemon(1)
+    .then( resp => console.log( resp ) )
+    .catch( error => console.log( error ) )
+    .finally( () => console.log('Fin del getPokemon()') );
