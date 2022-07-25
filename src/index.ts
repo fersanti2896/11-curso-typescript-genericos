81d9b945c1c36babaaa4a29ed7fb216d520565da
Fernando Nicolás <fersanti2896@gmail.com>
2022-07-25 02:08:45 -0500

    Función Genérica en generics.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { printObject, genericFunction, genericFunctionArrow } from "./generics/generics";
+import { Hero } from "./interfaces/hero";
+import { Villain } from './interfaces/villain';
 
-// printObject( 123 );
-// printObject( new Date() );
-// printObject( { a:1, b:2, c:3 } );
-// printObject( [1, 2, 3, 4, 5, 6] );
-// printObject( 'Hola Mundo!' );
+/* printObject( 123 );
+printObject( new Date() );
+printObject( { a:1, b:2, c:3 } );
+printObject( [1, 2, 3, 4, 5, 6] );
+printObject( 'Hola Mundo!' ); */
 
-console.log( genericFunction(3.1416).toFixed(2) );
-console.log( genericFunction('Hola mundo').toLocaleUpperCase() );
-console.log( genericFunction( new Date() ) );
+/* console.log( genericFunctionArrow(3.1416).toFixed(2) );
+console.log( genericFunctionArrow('Hola mundo').toLocaleUpperCase() );
+console.log( genericFunctionArrow( new Date() ) ); */
 
-console.log( genericFunctionArrow('Fernando').toUpperCase() );
+const deadpool = {
+    name: 'Deadpool',
+    realName: 'Wade Wilson', 
+    dangerLevel: 130
+}
+
+console.log( genericFunctionArrow<Hero>(deadpool) );
